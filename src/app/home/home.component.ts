fb899d9de05952e7e038b3a13b860fb51513272a
Ioannis Zarlas <ioannis.zarlas@chromaway.com>
2025-07-04 13:28:53 +0300

    Added search bar

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,12 +4,13 @@ import { Component } from '@angular/core';
   selector: 'app-home',
   imports: [],
   template: `
-    <p>
-      home works!
-    </p>
+    <section>
+      <form>
+        <input type="text" placeholder="Filter by city" />
+        <button class="primary" type="button">Search</button>
+      </form>
+    </section>
   `,
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
