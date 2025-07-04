4e1c1069cbe6c1f6d787010ea5dff1d78e744df4
Ioannis Zarlas <ioannis.zarlas@chromaway.com>
2025-07-04 17:29:17 +0300

    Added basic ngFor and two way binding parent-child

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,16 +1,67 @@
 import { Component } from '@angular/core';
+import { HousingLocationComponent } from '../housing-location/housing-location.component';
+import { HousingLocation } from '../housingLocation';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CommonModule, HousingLocationComponent],
   template: `
     <section>
       <form>
         <input type="text" placeholder="Filter by city" />
         <button class="primary" type="button">Search</button>
       </form>
+      <app-housing-location
+        *ngFor="let housingLocation of housingLocationList"
+        [housingLocation]="housingLocation"
+      >
+      </app-housing-location>
     </section>
   `,
   styleUrl: './home.component.scss',
 })
-export class HomeComponent {}
+export class HomeComponent {
+  housingLocationList: HousingLocation[] = [
+    {
+      id: 0,
+      name: 'House one',
+      city: 'Chicago',
+      state: 'IL',
+      photo: '/assets/',
+      availableUnits: 4,
+      wifi: true,
+      laundry: true,
+    },
+    {
+      id: 1,
+      name: 'House two',
+      city: 'Chicago',
+      state: 'IL',
+      photo: '/assets/',
+      availableUnits: 2,
+      wifi: true,
+      laundry: false,
+    },
+    {
+      id: 2,
+      name: 'House three',
+      city: 'Chicago',
+      state: 'IL',
+      photo: '/assets/',
+      availableUnits: 3,
+      wifi: false,
+      laundry: true,
+    },
+    {
+      id: 3,
+      name: 'House four',
+      city: 'Chicago',
+      state: 'IL',
+      photo: '/assets/',
+      availableUnits: 1,
+      wifi: false,
+      laundry: false,
+    },
+  ];
+}
--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,15 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { HousingLocation } from '../housingLocation';
 
 @Component({
   selector: 'app-housing-location',
   imports: [],
   template: `
-    <p>
-      housing-location works!
-    </p>
+    <section>
+      <img
+        class="listing-photon"
+        [src]="[housingLocation.photo]"
+        alt="Exterior photo of {{ housingLocation.name }}"
+      />
+      <h2 class="listing-heading">{{ housingLocation.name }}</h2>
+      <p class="listing-location">
+        {{ housingLocation.city }}, {{ housingLocation.state }}
+      </p>
+    </section>
   `,
-  styleUrl: './housing-location.component.scss'
+  styleUrl: './housing-location.component.scss',
 })
 export class HousingLocationComponent {
-
+  @Input() housingLocation!: HousingLocation;
 }
