109da131092d23d4dd1add2df6cb9c029609abba
Ioannis Zarlas <ioannis.zarlas@chromaway.com>
2025-07-12 20:18:58 +0300

    Nest swagger api-docs setup

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,19 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle("Real Estate API documentation")
+    .setDescription("API documentation for the Real Estate endpoints")
+    .setVersion("1.0.0")
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("api-docs", app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
