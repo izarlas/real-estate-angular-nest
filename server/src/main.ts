80585b0277c55f3eb9b460b90fd25ea383ca24ab
Ioannis Zarlas <ioannis.zarlas@chromaway.com>
2025-07-12 19:44:03 +0300

    Server and monorepo setup finished

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,26 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { join } from 'path';
-import { NestExpressApplication } from '@nestjs/platform-express';
-import { Request, Response } from 'express';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
-
-  app.useStaticAssets(join(__dirname, '..', '..', 'dist/my-app/browser'));
-  app.setBaseViewsDir(join(__dirname, '..', '..', 'dist/my-app/browser'));
-  app.setViewEngine('html');
-
-  app.use('*', (req: Request, res: Response) => {
-    const indexHtml = join(
-      __dirname,
-      '..',
-      '..',
-      'dist/my-app/browser/index.html',
-    );
-    res.sendFile(indexHtml);
-  });
-
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule);
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
