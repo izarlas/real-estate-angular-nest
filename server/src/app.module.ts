de280ae7351ee41e07ff4f9337be1218ecd9d397
Ioannis Zarlas <ioannis.zarlas@chromaway.com>
2025-07-14 14:01:23 +0300

    Fixed the backend so it now runs

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,20 +1,20 @@
-import { Module } from "@nestjs/common";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
-import { ConfigModule } from "@nestjs/config";
-import { TypeOrmModule } from "@nestjs/typeorm";
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { HouseModule } from './house/house.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRoot({
-      type: "postgres",
-      host: "localhost",
+      type: 'postgres',
+      host: process.env.DATABASE_HOST,
       port: 5432,
-      username: `${process.env.DATABASE_USERNAME}`,
-      password: `${process.env.DATABASE_PASSWORD}`,
-      database: `${process.env.DATABASE}`,
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE,
       autoLoadEntities: true,
       synchronize: true,
     }),
