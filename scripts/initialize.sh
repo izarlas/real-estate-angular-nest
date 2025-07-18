4ddbbc5322f1e53ccb2494629e6a47cba1ed6f9c
Ioannis Zarlas <ioannis.zarlas@chromaway.com>
2025-07-18 15:57:24 +0300

    Init script clean up

--- scripts/initialize.sh
+++ scripts/initialize.sh
@@ -2,15 +2,8 @@
 
 set -e
 
-echo "Cleaning old Docker containers and images"
+echo "Rebuilding and starting Docker containers"
 docker-compose down -v --rmi all --remove-orphans
-
-echo "Building and starting Docker containers using compose"
 docker-compose up -d --build
-
-echo "Waiting for Postgresql"
-until docker exec postgres pg_isready -U postgres > /dev/null 2>&1; do
-  sleep 2
-done
 
 echo "Finished Initialization"
